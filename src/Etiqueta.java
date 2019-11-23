18d718801a89740f7dd21e2aa2eddad6deccb9d2
Agustin Miguel <agustinmiguel96@gmail.com>
2019-11-23 12:57:08 -0300

    Finish

---
+++ src/Etiqueta.java
@@ -0,0 +1,10 @@
+
+public class Etiqueta {
+	protected CondicionPorVaca condicion;
+	protected String nombre;
+	public Etiqueta(String nombre, CondicionPorVaca condicion) {
+		super();
+		this.condicion = condicion;
+		this.nombre = nombre;
+	}
+}
--- src/Sistema.java
+++ src/Sistema.java
@@ -1,4 +1,17 @@
-
+import java.util.ArrayList;
 public class Sistema {
+	protected ArrayList<Etiqueta> etiquetas;
 
+	public Sistema(ArrayList<Etiqueta> etiquetas) {
+		this.etiquetas = etiquetas;
+	}
+	
+	public void seCumple(Animal v){
+		for(Etiqueta e: etiquetas){
+			if(e.condicion.seCumple(v)){
+				System.out.println("el animal es " + e.nombre);
+			}
+		}
+	}
 }
+
