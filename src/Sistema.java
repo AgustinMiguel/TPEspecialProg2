853a24de9efe6a242478a007cfc8d17a97871aba
EzeBalcaldi <42391879+EzeBalcaldi@users.noreply.github.com>
2019-11-23 20:30:27 -0300

    Update Sistema.java

--- src/Sistema.java
+++ src/Sistema.java
@@ -2,9 +2,11 @@ import java.util.ArrayList;
 public class Sistema {
 	protected ArrayList<Etiqueta> etiquetas;
 
-	public Sistema(ArrayList<Etiqueta> etiquetas) {
-		this.etiquetas = etiquetas;
+	public Sistema() {
+		this.etiquetas = new ArrayList<Etiqueta>();
 	}
+	public void add(Etiqueta e) {
+		etiquetas.add(e);
 	
 	public void seCumple(Animal v){
 		for(Etiqueta e: etiquetas){
